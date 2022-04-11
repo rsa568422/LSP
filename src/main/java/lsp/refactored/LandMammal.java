46d7ac54a4e0a54920bf7f43fe78238a4ba7bb6c
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-04-11 20:00:10 +0200

    Fin de la práctica.
    Se refactorizan las clases Mammal, Dog y Dolphin para cumplir el principio de LSP.

---
+++ src/main/java/lsp/refactored/LandMammal.java
@@ -0,0 +1,9 @@
+package lsp.refactored;
+
+public class LandMammal extends Mammal {
+
+    public void walk() {
+        System.out.println("I am walking");
+    }
+
+}
---
+++ src/main/java/lsp/refactored/Mammal.java
@@ -0,0 +1,12 @@
+package lsp.refactored;
+
+import lombok.Getter;
+
+@Getter
+public class Mammal {
+
+    private Integer weight;
+
+    private Integer ageInDays;
+
+}
